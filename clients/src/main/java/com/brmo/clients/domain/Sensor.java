090e6f880dfebf2aa5184e1c7311170f48343062
Felipeecp <felipe.engcp@gmail.com>
2023-11-09 20:46:25 -0300

    atualizando client para conectar com front end

--- clients/src/main/java/com/brmo/clients/domain/Sensor.java
+++ clients/src/main/java/com/brmo/clients/domain/Sensor.java
@@ -4,10 +4,27 @@ import com.brmo.clients.domain.enumerated.Region;
 import com.brmo.clients.domain.enumerated.UF;
 import com.brmo.clients.dto.SensorRecord;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.lang.annotation.Documented;
+import java.time.LocalDate;
+
+@Document("sensor")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Sensor {
+    @Id
     private String codWMO;
-    private Region region;
+    private String cidade;
+    private Region regiao;
+    private String dataCriacao;
     private UF uf;
     private String latitude;
     private String longitude;
@@ -16,5 +33,4 @@ public class Sensor {
     public static Sensor of(SensorRecord record){
         return new ObjectMapper().convertValue(record, Sensor.class);
     }
-
 }
